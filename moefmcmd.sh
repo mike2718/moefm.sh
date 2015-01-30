f18ffb38ce34b2d053486752303565d371819332
kingdido999 <kingdido999@gmail.com>
2015-01-29 21:36:12 -0500

    handle empty song info; reorganize the code

--- moefmcmd.sh
+++ moefmcmd.sh
@@ -6,15 +6,27 @@
 # 历史:
 #   2013/10/12  Mike Akiba  初次发布
 
-#get_moefm_json () {
-#    curl -s -A moefmcmd.sh 'http://moe.fm/listen/playlist?api=json&api_key=4e229396346768a0c4ee2462d76eb0be052592bf8'
-#    return
-#}
+BASE_URL='http://moe.fm/listen/playlist?api=json&api_key='
+API_KEY='4e229396346768a0c4ee2462d76eb0be052592bf8'
+URL=$BASE_URL$API_KEY
+
+# 检查歌曲信息是否为空，若为空则以“未知”替代
+check_empty()
+{
+    local param=$1
+    local empty="未知"
+
+    if [ -z "$param" ]; then
+        param=$empty
+    fi
+
+    echo "$param"
+}
 
 while true; do
-    #number=$(curl -s -A moefmcmd.sh 'http://www.random.org/integers/?num=1&min=1&max=9&col=1&base=10&format=plain&rnd=new')
-    moefm_json=$(curl -s -A moefmcmd.sh 'http://moe.fm/listen/playlist?api=json&api_key=4e229396346768a0c4ee2462d76eb0be052592bf8')
+    moefm_json=$(curl -s -A moefmcmd.sh echo $URL)
     a=( {0..8} )
+
     # Fisher-Yates shuffle 算法
     for i in {8..1}
     do
@@ -24,18 +36,32 @@ while true; do
         a[$j]=${a[$i]}
         a[$i]=$tmp
     done
+
     for i in {0..8}
     do
         mp3_url=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].url")
+
         if [ "$mp3_url" == "null" ]; then
             echo "url为null"
             continue
         fi
+
         title=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].sub_title")
         artist=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].artist")
         album=$(echo $moefm_json | jq -M -r ".response.playlist[${a[$i]}].wiki_title")
+        
         clear
-        printf '艺术家: %s\n曲名:   %s\n专辑:   %s\n\n[SPACE] 暂停/继续 [q] 下一曲 [Ctrl-Z] 退出\n' "$artist" "$title" "$album"
+
+        title=$(check_empty $title)
+        artist=$(check_empty $artist)
+        album=$(check_empty $album)
+
+        printf '曲名:   %s\n' "$title"
+        printf '专辑:   %s\n' "$album"
+        printf '艺术家: %s\n' "$artist"
+        printf '\n'
+        printf '[SPACE] 暂停/继续 [q] 下一曲 [Ctrl-Z] 退出\n'
+        
         mpg123 -q -C "$mp3_url"
     done
 done
